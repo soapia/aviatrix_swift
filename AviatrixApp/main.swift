c3d8f9fec14679af112eb906f9f276c1c4eef3c5
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-06-10 18:07:30 -0600

    Initial Commit

---
+++ AviatrixApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AviatrixApp
+//
+//  Created by Amy Holt on 6/10/18.
+//  Copyright © 2018 Amy Holt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
